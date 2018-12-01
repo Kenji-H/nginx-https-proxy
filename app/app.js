14f5f4e618bf75d91e77d9d680863524f5e511fc
Kenji-H <kenji1226h@gmail.com>
2018-12-02 00:52:16 +0900

    アクセス元の情報をヘッダに付与するようにした

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@ var http = require('http');
 
 var server = http.createServer(
     function (request, response) {
-        util.log("request: %s %s", request.method, request.url);
+        util.log("request: %s %s %s", 
+        	request.method, request.url, JSON.stringify(request.headers));
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World!!');
         response.end();
