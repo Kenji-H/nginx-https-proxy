aefd54d10f9e98a88bebad180cccd0bb7d70778b
Kenji-H <kenji1226h@gmail.com>
2018-12-01 21:38:24 +0900

    nginxからnodeにプロキシーするサンプルを書いた

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var util = require('util');
+var http = require('http');
+
+var server = http.createServer(
+    function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World!!');
+        response.end();
+    }
+).listen(8080);
+util.log("Server started on port 8080");
