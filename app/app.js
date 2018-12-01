23ccbac66ffbde6d8a179c6395ffcfb9195e7171
Kenji-H <kenji1226h@gmail.com>
2018-12-01 21:58:12 +0900

    appを複数台にしてロードバランシングするようにした

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var http = require('http');
 
 var server = http.createServer(
     function (request, response) {
+        util.log("request: %s %s", request.method, request.url);
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World!!');
         response.end();
